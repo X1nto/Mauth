b947bc521215d4706c1b2529c4614d90a2ce0d65
X1nto <burtkni@yandex.ru>
2022-09-25 15:40:38 +0400

    Use ZxingDecoder

--- app/src/main/java/com/xinto/mauth/camera/analyzer/QrCodeAnalyzer.kt
+++ app/src/main/java/com/xinto/mauth/camera/analyzer/QrCodeAnalyzer.kt
@@ -1,16 +1,9 @@
 package com.xinto.mauth.camera.analyzer
 
-import android.graphics.ImageFormat
-import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
-import com.google.zxing.BarcodeFormat
-import com.google.zxing.BinaryBitmap
-import com.google.zxing.DecodeHintType
-import com.google.zxing.MultiFormatReader
 import com.google.zxing.NotFoundException
-import com.google.zxing.PlanarYUVLuminanceSource
-import com.google.zxing.common.HybridBinarizer
+import com.xinto.mauth.camera.decoder.ZxingDecoder
 import java.nio.ByteBuffer
 
 class QrCodeAnalyzer(
@@ -18,44 +11,17 @@ class QrCodeAnalyzer(
     private inline val onFail: (NotFoundException) -> Unit
 ) : ImageAnalysis.Analyzer {
 
-    private val reader = MultiFormatReader().apply {
-        setHints(
-            mapOf(
-                DecodeHintType.POSSIBLE_FORMATS to arrayListOf(BarcodeFormat.QR_CODE),
-            )
-        )
-    }
-
     override fun analyze(image: ImageProxy) {
         image.use { imageProxy ->
             val data = imageProxy.planes[0].buffer.toByteArray()
 
-            val source = PlanarYUVLuminanceSource(
-                /* yuvData = */
-                data,
-                /* dataWidth = */
-                imageProxy.width,
-                /* dataHeight = */
-                imageProxy.height,
-                /* left = */
-                0,
-                /* top = */
-                0,
-                /* width = */
-                imageProxy.width,
-                /* height = */
-                imageProxy.height,
-                /* reverseHorizontal = */
-                false,
+            ZxingDecoder.decodeYuvLuminanceSource(
+                data = data,
+                dataWidth = imageProxy.width,
+                dataHeight = imageProxy.height,
+                onSuccess = onSuccess,
+                onError = onFail
             )
-
-            val bitmap = BinaryBitmap(HybridBinarizer(source))
-
-            try {
-                onSuccess(reader.decodeWithState(bitmap))
-            } catch (e: NotFoundException) {
-                onFail(e)
-            }
         }
     }
 
