cea9fac77260f4c560bbddb595dbf0f0006c9b44
Yava <yava.coco@gmail.com>
2022-12-31 18:50:01 +0100

    Handle otpauth:// URIs
    * This feature is useful when a otp QR code is scanned from another App
    * Make getDigestFromUriAlgorithm case insensitive

--- app/src/main/java/com/xinto/mauth/MainActivity.kt
+++ app/src/main/java/com/xinto/mauth/MainActivity.kt
@@ -11,13 +11,19 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.with
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.xinto.mauth.domain.model.DomainAccountInfo
+import com.xinto.mauth.otp.parser.OtpUriParserError
+import com.xinto.mauth.otp.parser.OtpUriParserImpl
+import com.xinto.mauth.otp.parser.OtpUriParserResult
 import com.xinto.mauth.ui.navigation.MauthDestination
 import com.xinto.mauth.ui.screen.*
 import com.xinto.mauth.ui.theme.MauthTheme
@@ -49,13 +55,66 @@ class MainActivity : ComponentActivity() {
             }
             .launchIn(lifecycleScope)
 
+        // Parse Account to add, if there is one to add
+        var accountToAdd: DomainAccountInfo? = null
+        var parseError: OtpUriParserError? = null
+
+
+        if (intent?.scheme == "otpauth" && intent?.dataString != null) {
+            val parser = OtpUriParserImpl()
+
+            accountToAdd = when (val result = parser.parseOtpUri(intent?.dataString!!)) {
+                is OtpUriParserResult.Success ->
+                    DomainAccountInfo(
+                        id = null,
+                        icon = null,
+                        label = result.data.label,
+                        issuer = result.data.issuer,
+                        secret = result.data.secret,
+                        algorithm = result.data.algorithm,
+                        type = result.data.type,
+                        digits = result.data.digits,
+                        counter = result.data.counter ?: 0,
+                        period = result.data.period ?: 30,
+                    )
+                is OtpUriParserResult.Failure -> {
+                    parseError = result.error
+                    null
+                }
+            }
+        }
+
         setContent {
             MauthTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Main()
+                    // If there was an error parsing the URI, display an alert dialog
+                    if (parseError != null) {
+                        val showErrorDialog = remember { mutableStateOf(true) }
+                        if(showErrorDialog.value)
+                            AlertDialog(
+                                onDismissRequest = { showErrorDialog.value = false },
+                                confirmButton = {
+                                    TextButton(
+                                        onClick = { showErrorDialog.value = false }) {
+                                        Text(stringResource(android.R.string.ok))
+                                    }
+                                },
+                                title = {
+                                    Text(stringResource(R.string.urihandler_parseerror_dialog_title))
+                                },
+                                text = {
+                                    Text(String.format(
+                                        "%s: %s",
+                                        stringResource(R.string.urihandler_parseerror_dialog_body),
+                                        parseError.toString()
+                                    ))
+                                },
+                            )
+                    }
+                    Main(accountToAdd)
                 }
             }
         }
@@ -63,8 +122,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Main() {
+fun Main(accountToAdd: DomainAccountInfo?) {
     val navigator = rememberBackstackNavigator<MauthDestination>(MauthDestination.Home)
+
+    if (accountToAdd != null)
+        navigator.push(MauthDestination.AddAccount(accountToAdd))
+
     Taxi(
         navigator = navigator,
         transitionSpec = {
