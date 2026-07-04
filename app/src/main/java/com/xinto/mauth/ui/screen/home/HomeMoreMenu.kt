c5413d6a8de1c4ac4916e8c20ee3e875ab13bf1b
Xinto <tornike.khintibidze2@gmail.com>
2026-07-04 14:43:29 +0400

    Move settings below export

--- app/src/main/java/com/xinto/mauth/ui/screen/home/HomeMoreMenu.kt
+++ app/src/main/java/com/xinto/mauth/ui/screen/home/HomeMoreMenu.kt
@@ -9,7 +9,7 @@ enum class HomeMoreMenu(
     @param:StringRes val title: Int
 ) {
     Groups(R.drawable.ic_label, R.string.home_more_groups),
-    Settings(R.drawable.ic_settings, R.string.settings_title),
     Export(R.drawable.ic_export, R.string.export_title),
+    Settings(R.drawable.ic_settings, R.string.settings_title),
     About(R.drawable.ic_info, R.string.about_title)
 }
