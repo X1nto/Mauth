96bd7e6a8b90c0ce96b0474e431729119fd90c63
Xinto <tornike.khintibidze2@gmail.com>
2026-07-05 20:32:31 +0400

    Fix home screen FAB padding

--- app/src/main/java/com/xinto/mauth/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/xinto/mauth/ui/screen/home/HomeScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -323,6 +324,7 @@ fun HomeScreen(
                     )
                 } else {
                     AppBarWithSearch(
+                        modifier = Modifier.padding(top = 8.dp),
                         state = searchBarState,
                         inputField = searchInputField,
                         navigationIcon = if (!isExpandedWidth) null else { ->
@@ -339,6 +341,9 @@ fun HomeScreen(
         },
         floatingActionButton = {
             AnimatedVisibility(
+                // FloatingActionButtonMenu and Scaffold both add FAB paddings.
+                // This offsets one of them.
+                modifier = Modifier.offset(x = 16.dp, y = 16.dp),
                 visible = !isSelectionActive,
                 enter = scaleIn() + fadeIn(),
                 exit = scaleOut() + fadeOut()
