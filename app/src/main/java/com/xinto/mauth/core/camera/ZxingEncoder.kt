498c2917c3903be9c579b38487d4fbca21093373
Xinto <tornike.khintibidze2@gmail.com>
2026-08-28 16:15:05 +0400

    Make encodeToBitmap non-suspend

--- app/src/main/java/com/xinto/mauth/core/camera/ZxingEncoder.kt
+++ app/src/main/java/com/xinto/mauth/core/camera/ZxingEncoder.kt
@@ -2,45 +2,39 @@ package com.xinto.mauth.core.camera
 
 import android.graphics.Bitmap
 import androidx.annotation.ColorInt
+import androidx.core.graphics.createBitmap
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.google.zxing.MultiFormatWriter
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
-import androidx.core.graphics.createBitmap
-import androidx.core.graphics.set
 
 object ZxingEncoder {
 
     private val writer = MultiFormatWriter()
 
-    suspend fun encodeToBitmap(
+    fun encodeToBitmap(
         data: String,
         size: Int,
         @ColorInt backgroundColor: Int,
         @ColorInt dataColor: Int
     ): Bitmap {
-        return withContext(Dispatchers.IO) {
-            suspendCoroutine { continuation ->
-                val bitMatrix = writer.encode(
-                    /* contents = */ data,
-                    /* format = */ BarcodeFormat.QR_CODE,
-                    /* width = */ size,
-                    /* height = */ size,
-                    /* hints = */ mapOf(EncodeHintType.MARGIN to 2)
-                )
-                val bitmap = createBitmap(size, size).apply {
-                    for (x in 0 until size) {
-                        for (y in 0 until size) {
-                            val hasData = bitMatrix.get(x, y)
-                            this[x, y] = if (hasData) dataColor else backgroundColor
-                        }
-                    }
-                }
-                continuation.resume(bitmap)
+        val bitMatrix = writer.encode(
+            /* contents = */ data,
+            /* format = */ BarcodeFormat.QR_CODE,
+            /* width = */ size,
+            /* height = */ size,
+            /* hints = */ mapOf(EncodeHintType.MARGIN to 2)
+        )
+
+        val pixels = IntArray(size * size)
+        for (y in 0 until size) {
+            val row = y * size
+            for (x in 0 until size) {
+                pixels[row + x] = if (bitMatrix.get(x, y)) dataColor else backgroundColor
             }
+        }
+
+        return createBitmap(size, size).apply {
+            setPixels(pixels, 0, size, 0, 0, size, size)
         }
     }
 }
